0d99064ee90da93b91326dc3c720e32690f654ab
VladilavOS <pochtachernysha@gmail.com>
2025-09-25 22:20:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def hello():
     return f"Hello {name}! {message}!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
